3957856f39f14eb5d4dc0455603d5b71931bc7e6
JetsemaniHidalgo <136549561+JetsemaniHidalgo@users.noreply.github.com>
2023-10-07 14:26:16 -0600

    Proyecto2.py

--- Proyecto2.py
+++ Proyecto2.py
@@ -17,25 +17,32 @@ casas['Departamento'] = label_encoder.fit_transform(casas['Departamento'])
 
 # Verificar el resultado
 print(casas.head())
+#0 es IT y 1 es RH
 
 # Crear gráficos de dispersión para ver la relación entre las variables numéricas y 'Valor_casa'
 variables_numericas = ['Salario', 'Hijos', 'Departamento']
 
-for variable in variables_numericas:
-    plt.figure(figsize=(10, 6))
-    sns.histplot(data=casas, x=variable, kde=True)
-    plt.title(f'Histograma de {variable}')
-    plt.xlabel(variable)
-    plt.ylabel('Frecuencia')
-    plt.grid(True)
-    plt.show()
+# Definir una paleta de colores personalizada con colores distintivos
+colores = ['#1f77b4', '#ff7f0e', '#2ca02c']  # Puedes agregar más colores si es necesario
 
-for variable in variables_numericas:
-    plt.figure(figsize=(10, 6))
-    sns.scatterplot(x=variable, y='Valor_casa', data=casas, alpha=0.5)
-    plt.title(f'Gráfico de dispersión entre {variable} y Valor_casa')
-    plt.grid(True)
-    plt.show()
+# Crear un conjunto de subplots para los gráficos
+fig, axes = plt.subplots(2, len(variables_numericas), figsize=(15, 8))
+
+# Crear gráficos de histogramas y dispersión con colores diferentes para cada variable
+for i, variable in enumerate(variables_numericas):
+    sns.histplot(data=casas, x=variable, kde=True, ax=axes[0, i], color=colores[i])
+    axes[0, i].set_title(f'Histograma de {variable}')
+    axes[0, i].set_xlabel(variable)
+    axes[0, i].set_ylabel('Frecuencia')
+    axes[0, i].grid(True)
+    
+    sns.scatterplot(x=variable, y='Valor_casa', data=casas, alpha=0.5, ax=axes[1, i], color=colores[i])
+    axes[1, i].set_title(f'Gráfico de dispersión entre {variable} y Valor_casa')
+    axes[1, i].grid(True)
+
+# Ajustar el espacio entre subplots
+plt.tight_layout()
+plt.show()
     
 # Calcular la matriz de correlación
 correlation_matrix = casas[variables_numericas + ['Valor_casa']].corr()
@@ -84,8 +91,8 @@ print(f"Error Cuadrático Medio (MSE): {mse_test}")
 print(f"Coeficiente de Determinación (R^2): {r2_test}")
 
 # Datos de los nuevos casos
-nuevo_caso1 = [[17000, 3, 1]]  # Primer caso con departamento codificado como 1
-nuevo_caso2 = [[15400, 1, 0]]  # Segundo caso con departamento codificado como 0
+nuevo_caso1 = [[17000, 3, 1]]  # Primer caso con departamento codificado como 1 (RH)
+nuevo_caso2 = [[15400, 1, 0]]  # Segundo caso con departamento codificado como 0 (IT)
 
 # Realizar predicciones para los nuevos casos
 prediccion_caso1 = modelo_regresion.predict(nuevo_caso1)
